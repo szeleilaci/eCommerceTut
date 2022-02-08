8c78a78de1788aa94f280b748f61f91f5b3409c1
Laszlo Szelei <szelei.laszlo.sl@gmail.com>
2022-02-08 23:18:48 +0100

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const HatsPage = () => (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='shop/hats' element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,20 +2,30 @@
 import react from "react";
 
 import "./menu-item.styles.scss";
+import { useNavigate } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();
+
+  const onBackClick = (e) => {
+    e.preventDefault();
+    navigate(`${linkUrl}`);
+  };
+
+  return (
+    <div className={`${size} menu-item`} onClick={onBackClick}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MenuItem;
