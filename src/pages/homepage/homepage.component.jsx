47545cb8e5f2e23578e24f1e48f5a83b0cbc8c73
Laszlo Szelei <szelei.laszlo.sl@gmail.com>
2022-02-08 19:37:47 +0100

    bunch of staff

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import react from "react";
+
+import Directory from "../../components/directory/directory.component";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
