ffde57d04087587dd337cc08c3d8a1ce5636b87e
Laszlo Szelei <szelei.laszlo.sl@gmail.com>
2022-02-13 22:30:19 +0100

    Adding header and page - home link

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shoppage.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/084 crown.svg";
+
+import "./header.styles.scss";
+
+const Header = () => (
+  <div className='header'>
+    <Link to={"/"} className={"logo-container"}>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to={"/shop"}>
+        SHOP
+      </Link>
+      <Link className='option' to={"/contact"}>
+        CONTACT
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
